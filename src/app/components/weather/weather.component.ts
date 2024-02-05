3306272bb5b8f308ee6c81d620f5d0af79d21c0b
Max Bykov <mbykov@transperfect.com>
2024-02-05 06:08:35 +0400

    f around and find out

--- src/app/components/weather/weather.component.ts
+++ src/app/components/weather/weather.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { WeatherService } from '../../services/weather.service';
 
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
-  styleUrls: ['./weather.component.css']
+  styleUrls: ['./weather.component.css'],
 })
 export class WeatherComponent implements OnInit {
+  hourlyForecast: any;
 
-  constructor() { }
+  constructor(private weatherService: WeatherService) {}
 
   ngOnInit(): void {
+    this.weatherService.getMinuteForecast().subscribe((data) => {
+      this.hourlyForecast = data?.data?.timelines[0]?.intervals;
+    });
   }
-
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,11 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class WeatherService {
+  private apiKey = 'MNB1N1K6222KFuzSXNU2ZIDsZzE0VjM4';
 
   constructor(private http: HttpClient) {}
 
+  getMinuteForecast(): Observable<any> {
+    const baseUrl = 'https://api.tomorrow.io/v4/timelines';
+    const location = 'tbilisi';
+    const startTime = new Date().toISOString();
+    const endTime = new Date(new Date().getTime() + 1 * 24 * 60 * 60 * 1000).toISOString();
+
+    const url = `${baseUrl}?location=${location}&fields=temperature&startTime=${startTime}&endTime=${endTime}&timesteps=1h`;
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'apikey': this.apiKey,
+    });
+
+    return this.http.get(url, { headers });
+  }
 }
